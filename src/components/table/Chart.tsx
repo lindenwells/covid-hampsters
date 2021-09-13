2735015e062fe0dab19fdfd85d474c34b7470100
Kevin <yuanbwen0314@163.com>
2021-09-13 19:44:10 +1000

    add basic regression tools to predict bed # in Chart.tsx

--- src/components/table/Chart.tsx
+++ src/components/table/Chart.tsx
@@ -8,6 +8,7 @@
  */
 
 import { ReferenceLine, Brush, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import regression from 'regression';
 
 export default function BedsRequiredChart() {
   return(
@@ -26,64 +27,113 @@ export default function BedsRequiredChart() {
         <YAxis />
         <Tooltip />
         <Legend />
-        <Line type="monotone" dataKey="Patients" stroke="#ff9800" activeDot={{ r: 8 }} />
-        <Line type="monotone" dataKey="Patients (Predicted)" stroke="#42a5f5" />
-        <ReferenceLine x="8/09/2021" stroke="#42a5f5" label="Today" />
+        <Line type="monotone" dataKey="patients" stroke="#ff9800" activeDot={{ r: 8 }} />
+        <Line type="monotone" dataKey="predicted" stroke="#42a5f5" />
+        <ReferenceLine x="12/09/2021" stroke="#42a5f5" label="Today" />
         <Brush dataKey="name" height={40} stroke="#8884d8"/>
       </LineChart>
     </ResponsiveContainer>
   );
 }
 
+type DataPoint = {
+  x : number,
+  name : string,
+  patients ?: number
+  predicted ?: number
+}
 // example data
-const data = [
+
+var data : DataPoint[] = [
   {
+    x: 1,
     name: '1/09/2021',
-    Patients: 4100,
+    patients: 4100,
   },
   {
+    x: 2,
     name: '2/09/2021',
-    Patients: 3200,
+    patients: 3200,
   },
   {
+    x: 3,
     name: '3/09/2021',
-    Patients: 1900,
+    patients: 1900,
   },
   {
+    x: 4,
     name: '4/09/2021',
-    Patients: 2800,
+    patients: 2800,
   },
   {
+    x: 5,
     name: '5/09/2021',
-    Patients: 1903,
+    patients: 1903,
   },
   {
+    x: 6,
     name: '6/09/2021',
-    Patients: 2306,
+    patients: 2306,
   },
   {
+    x: 7,
     name: '7/09/2021',
-    Patients: 3500,
+    patients: 3500,
   },
   {
+    x: 8,
     name: '8/09/2021',
-    Patients: 4000,
-    "Patients (Predicted)": 4000,
+    patients: 4000,
   },
   {
+    x: 9,
     name: '9/09/2021',
-    "Patients (Predicted)": 5444,
+    patients: 5444,
   },
   {
+    x: 10,
     name: '10/09/2021',
-    "Patients (Predicted)": 6000,
+    patients: 6000,
   },
   {
+    x: 11,
     name: '11/09/2021',
-    "Patients (Predicted)": 7444,
+    patients: 7444,
   },
   {
+    x: 12,
     name: '12/09/2021',
-    "Patients (Predicted)": 7000,
+    patients: 7000,
   },
 ];
+
+let twoDim = 
+  data.map(
+    (date) => [date.x, date.patients]
+  );
+
+  console.log(twoDim);
+
+const linearRegression = regression.polynomial(twoDim, {order : 3});
+
+const [,dataPrediction1] = linearRegression.predict(12);
+const [,dataPrediction2] = linearRegression.predict(13);
+const [,dataPrediction3] = linearRegression.predict(14);
+
+data = data.concat(
+  {
+    x: 13,
+    name:  '13/09/2021', 
+    predicted: dataPrediction1
+  },
+  {
+    x: 14,
+    name:  '14/09/2021', 
+    predicted: dataPrediction2
+  },
+  {
+    x: 15,
+    name:  '15/09/2021', 
+    predicted: dataPrediction3
+  }
+  );
