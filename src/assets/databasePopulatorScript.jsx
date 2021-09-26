378434cebeb63cceabdaad10b91f778ed5a3ca6d
Linden Wells <lindenwells9@gmail.com>
2021-09-26 21:54:32 +1000

    progress perhaps?

--- src/assets/databasePopulatorScript.jsx
+++ src/assets/databasePopulatorScript.jsx
@@ -1,6 +1,7 @@
 import { db } from "../firebase";
-import { data } from "./hospitals";
+import { data } from "./bedNumbers";
 import { Button } from "@material-ui/core";
+import { arrayUnion } from "@firebase/firestore";
 
 // Function to print all documents in the firestore "hospital" collection
 // This may serve as an example query too
@@ -18,7 +19,7 @@ const printHospitals = () => {
 };
 
 // Add this component and click it to add documents to firestore
-export const Populator = () => {
+export const DataPopulator = () => {
   return (
     <Button
       onClick={() => {
@@ -36,7 +37,26 @@ export const Populator = () => {
         })
       }}
     >
-      Click to add data to firestore
+      Click to add hospital data to firestore
+    </Button>
+  );
+};
+
+export const OccupancyPopulator = () => {
+  return (
+    <Button
+      onClick={() => {
+        db.collection("occupancy_data")
+          .add(arrayUnion(data))
+          .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+          })
+          .catch((error) => {
+            console.error("Error adding document: ", error);
+          })
+      }}
+    >
+      Click to add occupancy data to firestore
     </Button>
   );
 };
