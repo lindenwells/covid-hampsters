31e1a3c8864b26c824f28cb912ed385d5667ff79
Jason <jasonhassell228@gmail.com>
2021-09-27 16:06:28 +1000

    progress on merge from kevin (unfinished)

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,13 +1,19 @@
+import React, { useState } from "react";
 import firebase, { authUiConfig } from "../firebase";
-import { Button, Grid, TextField, InputAdornment } from "@material-ui/core";
+import { Button, Grid, TextField, InputAdornment, Input } from "@material-ui/core";
 import { StyledFirebaseAuth } from "react-firebaseui";
 import { AccountCircle, LockRounded } from "@material-ui/icons";
+
 type loginProps = {
   loggedIn: boolean;
 };
 
-// TODO: Feel free to play around with the style and make it look nice :)
 const Login = (props: loginProps) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
+
   if (props.loggedIn) {
     return (
       <Grid>
@@ -31,18 +37,48 @@ const Login = (props: loginProps) => {
             {/* <header className="App-header"> */}
             <p>You're logged out</p>
             <p>Sign In</p>
-            <TextField color="primary" label="Username" margin="normal" InputProps={{ startAdornment: <InputAdornment position="start"><AccountCircle /></InputAdornment> }} />
-            <TextField color="primary" label="Password" type="password" margin="normal" InputProps={{ startAdornment: <InputAdornment position="start"><LockRounded /></InputAdornment> }} />
+            <TextField 
+              color="primary"
+              label="Email"
+              margin="normal"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              InputProps={{
+                startAdornment: <InputAdornment position="start">
+                  <AccountCircle />
+                </InputAdornment>
+              }}
+            />
+
+            <TextField 
+              color="primary"
+              label="Password" 
+              type="password" 
+              margin="normal"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              InputProps={{ startAdornment: 
+              <InputAdornment position="start">
+                <LockRounded />
+              </InputAdornment>
+              }}
+            />
+
             <div style={{ height: 20 }} />
-            <Button variant="contained">
+            <Button 
+              variant="contained"
+              onClick={() => {
+                alert(email + ", " + password);
+                }}>
               Log In
             </Button>
             <div style={{ height: 20 }} />
-            <Button variant="contained">Sign Up</Button>
+
             <StyledFirebaseAuth
               uiConfig={authUiConfig}
               firebaseAuth={firebase.auth()}
             ></StyledFirebaseAuth>
+            <Button variant="contained">Register</Button>
             {/* </header> */}
           </Grid>
         </div>
