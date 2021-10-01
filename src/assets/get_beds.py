a62ba4c130f81c5a6a6089162e1b82cffb9c97c1
Linden Wells <lindenwells9@gmail.com>
2021-10-01 10:53:55 +1000

    Upload to firestore and add some (hopefully) helpful comments

--- src/assets/get_beds.py
+++ src/assets/get_beds.py
@@ -2,6 +2,19 @@ from typing import List
 import urllib3
 import json
 from random import randint
+from google.cloud import firestore #pip3 install google-cloud-firestore
+from datetime import date
+
+# See https://googleapis.dev/python/google-api-core/latest/auth.html
+# for how to authenticate to upload to firebase. Not needed if you just want to print results.
+
+# In particular, pip3 install google-cloud-firestore
+# $ export GOOGLE_APPLICATION_CREDENTIALS="/path/to/keyfile.json"
+
+# Example query:
+# users_ref = db.collection('users')
+# for doc in users_ref.stream():
+#     print(u'{} => {}'.format(doc.id, doc.to_dict()))
 
 BASE_URL = 'https://www.data.gov.au'
 
@@ -19,6 +32,11 @@ def get_known_hospitals(path : str) -> List[str]:
 def main():
     known = get_known_hospitals("./src/assets/hospitals.json")
     assert len(known) >= 1 # throw an error if known is empty
+
+    db = firestore.Client()
+    today = date.today().strftime("%Y-%m-%d") # should print in the format YYYY-MM-DD
+    doc_ref = db.collection('occupancy_data').document(today)
+
     http = urllib3.PoolManager()
     next_link = '/data/api/3/action/datastore_search?resource_id=43b9e4a4-0752-44c7-b825-bc32c46cf3b7'
     
@@ -41,8 +59,9 @@ def main():
     for hospital in hospitals:
         result[hospital["Hospital name"]] = hospital["Number of available beds"]
     
+    doc_ref.set(result)
     # TODO save result in firebase with the document ID as the current day
-    print(json.dumps(result, indent=4))
+    # print(json.dumps(result, indent=4))
 
 if __name__ == "__main__":
     main()
