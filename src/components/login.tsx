b144122e4d9d7ed01faff50aa7a9e47e1ad4df7f
Diya <sdy154229557@gmail.com>
2021-10-03 16:29:24 +0800

    s

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import firebase, { registerWithEmailAndPassword, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithGoogle } from "../firebase";
-import { Button, Grid, TextField, InputAdornment, Input } from "@material-ui/core";
-import { AccountCircle, LockRounded } from "@material-ui/icons";
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import { Button, Grid, TextField, InputAdornment, Input, Link } from "@material-ui/core";
+import { AccountCircleOutlined, LockOutlined, EmailOutlined, Lock} from "@material-ui/icons";
+import { makeStyles, createStyles, Theme, ThemeProvider, createTheme } from "@material-ui/core/styles";
+import { palette } from "@mui/system";
 // import Register from "./register";
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -11,8 +12,14 @@ const useStyles = makeStyles((theme: Theme) =>
       // TODO: Feel free to play around with the login buttons styling
       margin: theme.spacing(2),
     },
-  })
+  }),
 );
+
+const customTheme = createTheme({
+  palette: {
+    type: 'dark',
+  },
+});
 
 type loginProps = {
   loggedIn: boolean;
@@ -28,82 +35,8 @@ const IsRegister = () => {
 
   if (resetPass) {
     return (
-      <>
-      <TextField 
-        color="primary"
-        label="Email"
-        margin="normal"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        InputProps={{
-          startAdornment: <InputAdornment position="start">
-            <AccountCircle />
-          </InputAdornment>
-        }}
-      />
-      <Button className={classes.loginBtns} variant="contained" onClick={() => {sendPasswordResetEmail(email);}}>Send password reset email</Button>
-      <Button className={classes.loginBtns} variant="contained" onClick={() => {setResetPass(false);}}>Back to login</Button>
-      </>
-    )
-  } else if (registered) {
-    return (
-      <>
-      <form onSubmit={(e) => {e.preventDefault()}}> {/* Again this form allows submission on enter, div below needs styling */}
-        <div style={{ display: 'flex', flexDirection: 'column', maxWidth: 400, minWidth: 300 }}>
-          Register
-          <TextField 
-            color="primary"
-            label="Name"
-            margin="normal"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            InputProps={{
-              startAdornment: <InputAdornment position="start">
-                <AccountCircle />
-              </InputAdornment>
-            }}
-          />
-          <TextField 
-            color="primary"
-            label="Email"
-            margin="normal"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            InputProps={{
-              startAdornment: <InputAdornment position="start">
-                <AccountCircle />
-              </InputAdornment>
-            }}
-          />
-          <TextField 
-            color="primary"
-            label="Password" 
-            type="password"
-            margin="normal"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            InputProps={{ startAdornment: 
-            <InputAdornment position="start">
-              <LockRounded />
-            </InputAdornment>
-            }}
-          />
-          <Button className={classes.loginBtns} variant="contained" type="submit" onClick={() => {registerWithEmailAndPassword(name, email, password);}}>Register</Button>
-          </div>
-        </form>
-      <Button className={classes.loginBtns} variant="contained" onClick={() => {setRegistered(false);}}>Back to login</Button>
-      </>
-    );
-  } else {
-    return (
-      <>
-      <p>You're logged out</p>
-
-      <form onSubmit={(e) => {e.preventDefault()}}> {/* This form allows form submission on enter */}
-        <div> {/* This div keeps things in one column */}
-        {/* TODO: This div makes login too wide, not sure how to fix */}
-        <p>Sign In</p>
-        <TextField 
+      <ThemeProvider theme={customTheme}>
+        <TextField
           color="primary"
           label="Email"
           margin="normal"
@@ -111,48 +44,126 @@ const IsRegister = () => {
           onChange={(e) => setEmail(e.target.value)}
           InputProps={{
             startAdornment: <InputAdornment position="start">
-              <AccountCircle />
+              <EmailOutlined />
             </InputAdornment>
           }}
         />
+        <Button style={{ width: 250 }} className={classes.loginBtns} variant="contained" onClick={() => { sendPasswordResetEmail(email); }}>Send password reset email</Button>
+        <Button style={{ width: 250 }} className={classes.loginBtns} variant="contained" onClick={() => { setResetPass(false); }}>Back to login</Button>
+      </ThemeProvider>
+    )
+  } else if (registered) {
+    return (
+      <ThemeProvider theme={customTheme}>
+        <form onSubmit={(e) => { e.preventDefault() }}> {/* Again this form allows submission on enter, div below needs styling */}
+          <div style={{ display: 'flex', flexDirection: 'column', maxWidth: 400, minWidth: 300 }}>
+            Register
+            <TextField
+              color="primary"
+              label="Name"
+              margin="normal"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              InputProps={{
+                startAdornment: <InputAdornment position="start">
+                  <AccountCircleOutlined />
+                </InputAdornment>
+              }}
+            />
+            <TextField
+              label="Email"
+              margin="normal"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              InputProps={{
+                startAdornment: <InputAdornment position="start">
+                  <EmailOutlined />
+                </InputAdornment>
+              }}
+            />
+            <TextField
+              label="Password"
+              type="password"
+              margin="normal"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              InputProps={{
+                startAdornment:
+                  <InputAdornment position="start">
+                    <LockOutlined />
+                  </InputAdornment>
+              }}
+            />
+            <Button style={{ width: 250 }} className={classes.loginBtns} variant="contained" type="submit" onClick={() => { registerWithEmailAndPassword(name, email, password); }}>Register</Button>
+          </div>
+        </form>
+        <Button style={{ width: 250 }} className={classes.loginBtns} variant="contained" onClick={() => { setRegistered(false); }}>Back to login</Button>
+      </ThemeProvider>
+    );
+  } else {
+    return (
+      <ThemeProvider theme={customTheme}>
+        <p>You're logged out</p>
 
-        <TextField 
-          color="primary"
-          label="Password" 
-          type="password"
-          margin="normal"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          InputProps={{ startAdornment: 
-          <InputAdornment position="start">
-            <LockRounded />
-          </InputAdornment>
-          }}
-        />
-        </div>
-        <Button 
-          variant="contained"
-          type="submit"
-          // replace onClick func with email/pass firebase stuff
-          onClick={(e) => 
+        <form onSubmit={(e) => { e.preventDefault() }}> {/* This form allows form submission on enter */}
+          <div> {/* This div keeps things in one column */}
+            {/* TODO: This div makes login too wide, not sure how to fix */}
+            <h2>Sign In</h2>
+            <TextField
+              label="Email"
+              margin="normal"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              InputProps={{
+
+                startAdornment: <InputAdornment position="start">
+                  <EmailOutlined />
+                </InputAdornment>
+              }}
+            />
+
+            <TextField
+              label="Password"
+              type="password"
+              margin="normal"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              InputProps={{
+                startAdornment:
+                  <InputAdornment position="start">
+                    <LockOutlined />
+                  </InputAdornment>
+              }}
+            />
+          </div>
+          <Button
+            variant="contained"
+            type="submit"
+            // replace onClick func with email/pass firebase stuff
+            onClick={(e) =>
             (e.preventDefault,
-            signInWithEmailAndPassword(email, password))}
-          className={classes.loginBtns}
+              signInWithEmailAndPassword(email, password))}
+            className={classes.loginBtns}
+            style={{ width: 250 }}
           >
-          Log In
+            Log In
+          </Button>
+        </form>
+
+        <Button
+          variant="contained"
+          onClick={signInWithGoogle}
+          className={classes.loginBtns}
+          style={{ width: 250 }}>
+          Sign in with Google
         </Button>
-      </form>
-
-      <Button
-        variant="contained"
-        onClick={signInWithGoogle}
-        className={classes.loginBtns}>
-        Sign in with Google
-      </Button>
-
-      <Button className={classes.loginBtns} variant="contained" onClick={() => {setRegistered(true)}}>Create account</Button>
-      <Button className={classes.loginBtns} variant="contained" onClick={() => {setResetPass(true)}}>Forgot password? Click here</Button>
-      </>
+        <Link color="primary" href="#" variant="body2" onClick={() => { setResetPass(true) }}>
+          Forgot password?
+        </Link>
+        <Link color="primary" href="#" variant="body2" onClick={() => { setRegistered(true) }}>
+          {"Don't have an account? Sign Up"}
+        </Link>
+      </ThemeProvider>
     );
   }
 }
@@ -168,7 +179,7 @@ const Login = (props: loginProps) => {
             Welcome {firebase.auth()?.currentUser?.displayName}! You are now
             signed-in!
           </p>
-          <Button onClick={() => firebase?.auth()?.signOut()}>Sign-out</Button>
+          <Button style={{ width: 250 }} onClick={() => firebase?.auth()?.signOut()}>Sign-out</Button>
           {/* </header> */}
         </div>
       </Grid>
